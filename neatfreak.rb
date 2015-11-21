61c97566ef493fd694541c0692022eeb2aa73a42
Ellis Marte <elliscmarte@gmail.com>
2015-11-21 13:24:31 -0800

    MVP

--- neatfreak.rb
+++ neatfreak.rb
@@ -1,47 +1,21 @@
+require 'zlib'
 
-require 'find'
-FILES_SEARCHED = []
-
-def search_files_in_this_folder(folder)
+def find_all_files_that_should_be_compressed(folder)
+  # Changes to the directory desired
   Dir.chdir(folder)
+  # Grabs & stores all file paths 
   all_files_in_this_directory = Dir.glob("**/*")
   return if all_files_in_this_directory.empty?
-  all_files_in_this_directory.each do |file|
-    # begin
-    if File.file?(File.join(folder, file))
-      # p File.atime(file)
-      # File.atime(file) > Time.now - 60 * 60 * 24 * 14 ? p true : p false
-      # p file
-      # break if FILES_SEARCHED.include?(File.join(folder, file))
-      # FILES_SEARCHED << File.join(folder, file)
-      p 'file'
-    # elsif File.directory?(File.join(folder, file))
-    #   p 'folder'
-    #   search_files_in_this_folder(File.join(folder, file))
-    # else
-    #   next
-    # end
-    # rescue
-    #   next
+  # Iterates over file paths, checks if it's a file
+  # Sees if it has been accessed within last 2 weeks
+  all_files_in_this_directory.each do |path|
+    if File.file?(File.join(Dir.pwd, path)) && File.atime(path) < Time.now - 60 * 60 * 24 * 14 
+      # Create a compressed version of the file
+      File.write("#{path}.gz", Zlib.deflate(path))
+      # Delete the uncompressed file
+      File.unlink(path)
     end
   end
-  # return
 end
-search_files_in_this_folder("/")
 
-# def search_files_in_this_folder(folder)
-#   folder.each do |file|
-#     if file.type == folder
-#       search_files_in_this_folder(file)
-#     elsif file.last_opened < Date.one_week_ago 
-#       compress(file)
-#     else
-#       next
-#     end
-#     return 
-#   end
-# end
-
-# def compress(file)
-#   file.gzip
-# end
+find_all_files_that_should_be_compressed("/Users/Ellis/Desktop/test_for_nf")
