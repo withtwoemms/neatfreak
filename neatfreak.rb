6a67165aa4929095289702c9e70dc503137bad5e
Ellis Marte <elliscmarte@gmail.com>
2015-11-21 18:43:38 -0800

    Update neatfreak.rb

--- neatfreak.rb
+++ neatfreak.rb
@@ -34,7 +34,8 @@ def find_all_files_that_should_be_decompressed(folder)
       begin
         if File.extname(path) == ".gz"
             # Create a compressed version of the file
-            Zlib::GzipReader.open(path) { |gz| File.write("#{path}".chomp('.gz'), gz.read) }
+            # Zlib::GzipReader.open(path) { |gz| File.write("#{path}".chomp('.gz'), gz.read) }
+            Zlib::GzipReader.open(path)
             # Delete the uncompressed file
             File.unlink(path)
         end
