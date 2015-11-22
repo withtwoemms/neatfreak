e22a94bc2149872ba5c69f28f9f3b62152f3e178
Ellis Marte <elliscmarte@gmail.com>
2015-11-21 16:24:34 -0800

    Update neatfreak.rb

--- neatfreak.rb
+++ neatfreak.rb
@@ -21,3 +21,21 @@ end
 # replace the argument below with the folder that you want to target. 
 # to run this on your entire computer use "/" as the argument.
 # find_all_files_that_should_be_compressed("/neatfreak/test_folder")
+
+def find_all_files_that_should_be_decompressed(folder)
+    # Changes to the directory desired
+    Dir.chdir(folder)
+    # Grabs & stores all file paths
+    all_files_in_this_directory = Dir.glob("**/*")
+    return if all_files_in_this_directory.empty?
+    # Iterates over file paths, checks if it's a file
+    # Sees if it has been accessed within last 2 weeks
+    all_files_in_this_directory.each do |path|
+        if File.extname(path) == ".gz"
+            # Create a compressed version of the file
+            File.write("#{path.chomp('.gz')}", Zlib.inflate(path))
+            # Delete the uncompressed file
+            File.unlink(path)
+        end
+    end
+end
